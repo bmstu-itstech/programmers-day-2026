c4354f3a47e8c09a5553af0d43260f829b426403
zhikh <zhikh.k@gmail.com>
2026-09-04 20:03:31 +0300

    feat: add CI

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,26 @@
 import { Module } from "@nestjs/common";
 import { FormsModule } from "./forms/forms.module.js";
 import { SequelizeModule } from "@nestjs/sequelize";
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Form } from "./forms/models/form.model.js";
 
 @Module({
   imports: [
-    SequelizeModule.forRoot({
-      database: "progday",
-      dialect: "postgres",
-      username: "progday",
-      password: "s3cr3tpw",
-      host: "localhost",
-      port: 5432,
-      models: [Form],
-      synchronize: true,
-      autoLoadModels: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (cfg: ConfigService) => ({
+        dialect: 'postgres',
+        host: cfg.get('DB_HOST'),
+        port: cfg.get('DB_PORT'),
+        username: cfg.get('DB_USERNAME'),
+        password: cfg.get('DB_PASSWORD'),
+        database: cfg.get('DB_DATABASE'),
+        models: [Form],
+      }),
+      inject: [ConfigService],
     }),
     FormsModule,
   ],
