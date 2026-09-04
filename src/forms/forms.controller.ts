cf65e4d6a232262680f378d4c78b3f3b8a4df72f
zhikh <zhikh.k@gmail.com>
2026-09-04 22:25:05 +0300

    feat: add creating response

--- src/forms/forms.controller.ts
+++ src/forms/forms.controller.ts
@@ -1,13 +1,19 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { FormsService } from "./forms.service.js";
 import { CreateFormDTO } from "./dto/create-form.dto.js";
+import { ICreateResponse } from "./interfaces/create-response.interface.js";
 
 @Controller("forms")
 export class FormsController {
   constructor(private readonly formsService: FormsService) {}
 
   @Post()
-  async create(@Body() dto: CreateFormDTO) {
-    await this.formsService.create(dto);
+  async create(@Body() dto: CreateFormDTO): Promise<ICreateResponse> {
+    const form = await this.formsService.create(dto);
+    return {
+      id: form.id,
+      status: 'confirmed',
+      created_at: form.createdAt,
+    }
   }
 }
---
+++ src/forms/interfaces/create-response.interface.ts
@@ -0,0 +1,5 @@
+export interface ICreateResponse {
+  id: string
+  status: string
+  created_at: Date
+}
